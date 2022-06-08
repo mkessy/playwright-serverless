e86c9e692609526d65229629351a5743fe50e497
mkessy <kokokessy@gmail.com>
2022-06-07 17:41:00 -0700

    working playwright in aws node lambda docker container

--- render-screenshot/index.ts
+++ render-screenshot/index.ts
@@ -1,12 +1,24 @@
 // Create clients and set shared const values outside of the handler.
+import chromeAwsLambda from "chrome-aws-lambda";
+import playwright from "playwright-core";
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
 exports.renderScreenshot = async (event: unknown) => {
+  const browser = await playwright.chromium.launch({
+    args: chromeAwsLambda.args,
+    executablePath: await chromeAwsLambda.executablePath,
+    headless: chromeAwsLambda.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com");
+  const title = await page.evaluate(() => document.title);
+
   const response = {
     statusCode: 200,
-    body: "hello",
+    body: title,
   };
 
   // All log statements are written to CloudWatch
