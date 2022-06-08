ceed086f175bff32bbd1aa318d6d722d659b1c86
mkessy <kokokessy@gmail.com>
2022-06-07 19:23:48 -0700

    added test event

--- render-screenshot/dist/index.js
+++ render-screenshot/dist/index.js
@@ -1,12 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // Create clients and set shared const values outside of the handler.
+const chrome_aws_lambda_1 = __importDefault(require("chrome-aws-lambda"));
+const playwright_core_1 = __importDefault(require("playwright-core"));
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
 exports.renderScreenshot = async (event) => {
+    const browser = await playwright_core_1.default.chromium.launch({
+        args: chrome_aws_lambda_1.default.args,
+        executablePath: await chrome_aws_lambda_1.default.executablePath,
+        headless: chrome_aws_lambda_1.default.headless,
+    });
+    const { url } = event.data;
+    const page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    const title = await page.evaluate(() => document.title);
     const response = {
         statusCode: 200,
-        body: "hello",
+        body: title,
     };
     // All log statements are written to CloudWatch
     console.info(`response from: ${event} statusCode: ${response.statusCode} body: ${response.body}`);
--- render-screenshot/index.ts
+++ render-screenshot/index.ts
@@ -1,19 +1,36 @@
 // Create clients and set shared const values outside of the handler.
 import chromeAwsLambda from "chrome-aws-lambda";
 import playwright from "playwright-core";
+import {
+  Context,
+  APIGatewayProxyResult,
+  APIGatewayEvent,
+  APIGatewayProxyHandler,
+} from "aws-lambda";
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-exports.renderScreenshot = async (event: unknown) => {
+const renderScreenshot: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
   const browser = await playwright.chromium.launch({
     args: chromeAwsLambda.args,
     executablePath: await chromeAwsLambda.executablePath,
     headless: chromeAwsLambda.headless,
   });
 
+  if (!event.body) {
+    return {
+      body: "Error: No Url Provided",
+      statusCode: 403,
+    };
+  }
+  const { url } = JSON.parse(event.body);
+
   const page = await browser.newPage();
-  await page.goto("https://www.google.com");
+  await page.goto(url);
   const title = await page.evaluate(() => document.title);
 
   const response = {
@@ -27,3 +44,5 @@ exports.renderScreenshot = async (event: unknown) => {
   );
   return response;
 };
+
+exports.renderScreenshot = renderScreenshot;
